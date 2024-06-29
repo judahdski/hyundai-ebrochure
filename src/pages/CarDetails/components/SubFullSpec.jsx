278324c2791a1fd117ad1a3ea264fdd1af51b545
imJudah <judahjmdasuki@gmail.com>
2024-06-30 03:27:03 +0700

    Feat: membuat full spec page untuk desktop

--- src/pages/CarDetails/components/SubFullSpec.jsx
+++ src/pages/CarDetails/components/SubFullSpec.jsx
@@ -1,7 +1,82 @@
-import React from 'react';
+import { Dropdown, Table } from 'flowbite-react';
+import React, { useState } from 'react';
 
 const SubFullSpec = () => {
-	return <div>SubFullSpec</div>;
+	const [carModel, setCarModel] = useState(null);
+
+	return (
+		<div className='p-[72px] pt-0 flex flex-col gap-[32px]'>
+			{/*  */}
+			<div className='pt-[48px] flex items-center gap-6'>
+				<p className='font-medium'>Model</p>
+				<div className='py-2 px-4 rounded-lg border border-[#CFCFCF]'>
+					<Dropdown
+						label={carModel ?? 'Silahkan pilih model mobil'}
+						dismissOnClick={true}
+						inline>
+						<Dropdown.Item onClick={() => setCarModel('Dashboard')}>Dashboard</Dropdown.Item>
+						<Dropdown.Item onClick={() => setCarModel('Settings')}>Settings</Dropdown.Item>
+						<Dropdown.Item onClick={() => setCarModel('Earnings')}>Earnings</Dropdown.Item>
+						<Dropdown.Item onClick={() => setCarModel('Sign out')}>Sign out</Dropdown.Item>
+					</Dropdown>
+				</div>
+			</div>
+			<div className='flex flex-col gap-6'>
+				<p className='text-2xl text-center font-medium'>Performance</p>
+				<Table>
+					<Table.Head>
+						<Table.HeadCell className='bg-[#30486B] w-[30%] text-white'>Product name</Table.HeadCell>
+						<Table.HeadCell className='bg-[#30486B] w-[70%] text-white'></Table.HeadCell>
+					</Table.Head>
+					<Table.Body className='divide-y'>
+						<Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+							<Table.Cell className='whitespace-nowrap font-medium text-gray-900'>Engine Type</Table.Cell>
+							<Table.Cell>R 2.2 CRDi</Table.Cell>
+						</Table.Row>
+						<Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+							<Table.Cell className='whitespace-nowrap font-medium text-gray-900'>Displacement (cc)</Table.Cell>
+							<Table.Cell>2,199</Table.Cell>
+						</Table.Row>
+					</Table.Body>
+				</Table>
+				<Table>
+					<Table.Head>
+						<Table.HeadCell className='bg-[#30486B] w-[30%] text-white'>Product name</Table.HeadCell>
+						<Table.HeadCell className='bg-[#30486B] w-[70%] text-white'></Table.HeadCell>
+					</Table.Head>
+					<Table.Body className='divide-y'>
+						<Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+							<Table.Cell className='whitespace-nowrap font-medium text-gray-900'>Engine Type</Table.Cell>
+							<Table.Cell>R 2.2 CRDi</Table.Cell>
+						</Table.Row>
+						<Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+							<Table.Cell className='whitespace-nowrap font-medium text-gray-900'>Displacement (cc)</Table.Cell>
+							<Table.Cell>2,199</Table.Cell>
+						</Table.Row>
+					</Table.Body>
+				</Table>
+			</div>
+			<div className='flex flex-col gap-6'>
+				<p className='text-2xl text-center font-medium'>Dimensi</p>
+				<Table>
+					<Table.Head>
+						<Table.HeadCell className='bg-[#30486B] w-[30%] text-white'>Product name</Table.HeadCell>
+						<Table.HeadCell className='bg-[#30486B] w-[70%] text-white'></Table.HeadCell>
+					</Table.Head>
+					<Table.Body className='divide-y'>
+						<Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+							<Table.Cell className='whitespace-nowrap font-medium text-gray-900'>Engine Type</Table.Cell>
+							<Table.Cell>R 2.2 CRDi</Table.Cell>
+						</Table.Row>
+						<Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+							<Table.Cell className='whitespace-nowrap font-medium text-gray-900'>Displacement (cc)</Table.Cell>
+							<Table.Cell>2,199</Table.Cell>
+						</Table.Row>
+					</Table.Body>
+				</Table>
+			</div>
+		</div>
+	);
 };
 
 export default SubFullSpec;

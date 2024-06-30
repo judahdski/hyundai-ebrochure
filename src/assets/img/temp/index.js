44f79751e5acc85ad528bb3028c9a6d980c34e9a
imJudah <judahjmdasuki@gmail.com>
2024-06-30 17:36:25 +0700

    Feat: membuat gallery page untuk desktop

--- src/assets/img/temp/index.js
+++ src/assets/img/temp/index.js
@@ -2,5 +2,9 @@ import articleSect11Img from './article_sect1_1_img.png';
 import articleSect21Img from './article_sect2_1_img.png';
 import articleSect22Img from './article_sect2_2_img.png';
 import articleSect23Img from './article_sect2_3_img.png';
+import ionic6ColorPick from './ionic6_color_pick.png';
+import ionic6GalleryMain from './ionic6_gallery_main.png';
+import ionic6EksteriorOverview from './ionic6_eksterior_overview_bg.png';
+import ionic6InteriorOverview from './ionic6_interior_overview_bg.png';
 
-export { articleSect11Img, articleSect21Img, articleSect22Img, articleSect23Img };
+export { articleSect11Img, articleSect21Img, articleSect22Img, articleSect23Img, ionic6ColorPick, ionic6GalleryMain, ionic6EksteriorOverview, ionic6InteriorOverview };
--- src/pages/CarDetails/components/SubGallery.jsx
+++ src/pages/CarDetails/components/SubGallery.jsx
@@ -1,7 +1,53 @@
 import React from 'react';
+import { ionic6ColorPick, ionic6EksteriorOverview, ionic6GalleryMain, ionic6InteriorOverview } from '../../../assets/img/temp/index';
 
 const SubGallery = () => {
-	return <div>SubGallery</div>;
+	return (
+		<div className='p-[72px] pt-[48px] flex flex-col gap-[72px]'>
+			<div className='flex flex-col items-center gap-[32px]'>
+				<img
+					src={ionic6ColorPick}
+					alt='Ionic 6'
+					className='w-full max-w-[1000px]'
+				/>
+				<div className='w-full flex gap-[80px]'>
+					<div className='flex flex-col gap-4'>
+						<p className='text-2xl text-[#7C7C7C] font-medium'>Warna</p>
+						<p className='text-2xl font-semibold'>Biophilic Blue Pearl</p>
+					</div>
+					<div className='flex gap-6'>
+						<div className='h-full aspect-square bg-black rounded-2xl cursor-pointer'></div>
+						<div className='h-full aspect-square bg-red-500 rounded-2xl cursor-pointer'></div>
+						<div className='h-full aspect-square bg-blue-600 rounded-2xl cursor-pointer'></div>
+					</div>
+				</div>
+			</div>
+			<div className='flex flex-col gap-8'>
+				<h3 className='text-[32px] text-center font-medium'>Electrified streamliner.</h3>
+				<p className='text-center'>Perpaduan estetik dan fungsional yang menyatu secara harmonis dalam IONIQ 6 yang membentuk siluet aerodinamis dapat mengurangi hambatan udara, sehingga berkendara lebih halus dan dinamis.</p>
+				<img
+					src={ionic6GalleryMain}
+					alt='Ionic 6'
+				/>
+			</div>
+			<div className='flex flex-col gap-8'>
+				<div className='w-full h-[154px] rounded-2xl relative'>
+					<img
+						src={ionic6EksteriorOverview}
+						alt='Ionic 6 Eksterior Overview'
+					/>
+					<p className='absolute inset-y-0 w-full flex justify-center items-center text-[32px] text-white tracking-wide font-bold cursor-pointer'>Eksterior</p>
+				</div>
+				<div className='w-full h-[154px] rounded-2xl relative'>
+					<img
+						src={ionic6InteriorOverview}
+						alt='Ionic 6 Interior Overview'
+					/>
+					<p className='absolute inset-y-0 w-full flex justify-center items-center text-[32px] text-white tracking-wide font-bold cursor-pointer'>Interior</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default SubGallery;

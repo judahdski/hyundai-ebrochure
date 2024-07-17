34063cab9790143a7ab491c1676d77ea0e143054
judah-ims <judah@ims-tec.com>
2024-07-17 17:15:27 +0700

    Feat: membuat wabutton component

---
+++ src/components/WAButton.jsx
@@ -0,0 +1,10 @@
+export const WAButton = ({ content, customClass, waMessage }) => {
+  return (
+    <a
+      href={`https://wa.me/+62813131313?text=${encodeURIComponent(waMessage)}`}
+      className={`${customClass}`}
+    >
+      {content}
+    </a>
+  );
+};

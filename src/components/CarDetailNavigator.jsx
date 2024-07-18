54b1063743c0cecba9a6fc344f2aa4b056c9c804
imJudah <judahjmdasuki@gmail.com>
2024-07-18 23:36:27 +0700

    Merge2

--- src/components/CarDetailNavigator.jsx
+++ src/components/CarDetailNavigator.jsx
@@ -9,64 +9,52 @@ const CarDetailNavigator = ({ carDetailSection, setCarDetailSection }) => {
 	];
 	const { innerWidth: screenWidth } = window;
 
-  return screenWidth > 980 ? (
-    <div className='bg-white -mt-[27px] w-full flex justify-stretch rounded-[12px] overflow-hidden shadow-md'>
-      {navigatorList.map(({ code, title, isLast }, index) => (
-        <div
-          key={index}
-          className={`p-4 text-center tracking-[3px] ${
-            carDetailSection == code
-              ? ' text-[18px] text-[#1C4682] font-medium'
-              : 'text-black'
-          } flex-1 flex justify-center items-center  border-r-2 ${
-            !isLast ? 'border-[#EDF3FA]' : ''
-          } cursor-pointer hover:bg-slate-50`}
-          onClick={() => setCarDetailSection(code)}
-        >
-          {title}
-        </div>
-      ))}
-    </div>
-  ) : (
-    <>
-      <div className='bg-white -mt-[27px] w-full flex justify-center rounded-[12px] overflow-hidden shadow-md'>
-        <div
-          className={`p-4 text-center tracking-[3px] ${
-            carDetailSection == code
-              ? ' text-[18px] text-[#1C4682] font-medium'
-              : 'text-black'
-          } flex-1 flex justify-center items-center  border-r-2 ${
-            !isLast ? 'border-[#EDF3FA]' : ''
-          } cursor-pointer hover:bg-slate-50`}
-          onClick={() => setOpenModal(true)}
-        >
-          {title}
-        </div>
-      </div>
+	return screenWidth > 980 ? (
+		<div className='bg-white -mt-[27px] w-full flex justify-stretch rounded-[12px] overflow-hidden shadow-md'>
+			{navigatorList.map(({ code, title, isLast }, index) => (
+				<div
+					key={index}
+					className={`p-4 text-center tracking-[3px] ${carDetailSection == code ? ' text-[18px] text-[#1C4682] font-medium' : 'text-black'} flex-1 flex justify-center items-center  border-r-2 ${
+						!isLast ? 'border-[#EDF3FA]' : ''
+					} cursor-pointer hover:bg-slate-50`}
+					onClick={() => setCarDetailSection(code)}>
+					{title}
+				</div>
+			))}
+		</div>
+	) : (
+		<>
+			<div className='bg-white -mt-[27px] w-full flex justify-center rounded-[12px] overflow-hidden shadow-md'>
+				<div
+					className={`p-4 text-center tracking-[3px] ${carDetailSection == code ? ' text-[18px] text-[#1C4682] font-medium' : 'text-black'} flex-1 flex justify-center items-center  border-r-2 ${
+						!isLast ? 'border-[#EDF3FA]' : ''
+					} cursor-pointer hover:bg-slate-50`}
+					onClick={() => setOpenModal(true)}>
+					{title}
+				</div>
+			</div>
 
-      <Modal
-        show={openModal}
-        onClose={() => setOpenModal(false)}
-      >
-        <Modal.Body>
-          <div className='space-y-6'>
-            {navigatorList.map(({ code, title }, index) => (
-              <div
-                key={index}
-                className='p-6 bg-[#1C4682] w-full rounded-2xl flex justify-center text-white tracking-[6px]'
-                onClick={() => {
-                  setCarDetailSection(code);
-                  setOpenModal(false);
-                }}
-              >
-                {title.toUpperCase()}
-              </div>
-            ))}
-          </div>
-        </Modal.Body>
-      </Modal>
-    </>
-  );
+			<Modal
+				show={openModal}
+				onClose={() => setOpenModal(false)}>
+				<Modal.Body>
+					<div className='space-y-6'>
+						{navigatorList.map(({ code, title }, index) => (
+							<div
+								key={index}
+								className='p-6 bg-[#1C4682] w-full rounded-2xl flex justify-center text-white tracking-[6px]'
+								onClick={() => {
+									setCarDetailSection(code);
+									setOpenModal(false);
+								}}>
+								{title.toUpperCase()}
+							</div>
+						))}
+					</div>
+				</Modal.Body>
+			</Modal>
+		</>
+	);
 };
 
 export default CarDetailNavigator;
--- src/pages/CarDetails/components/SubFullSpec.jsx
+++ src/pages/CarDetails/components/SubFullSpec.jsx
@@ -2,15 +2,15 @@ import { Table } from 'flowbite-react';
 import React, { useState } from 'react';
 
 const SubFullSpec = ({ carID }) => {
-  const [carModel, setCarModel] = useState(null);
+	const [carModel, setCarModel] = useState(null);
 
-  //   const carDetail = carDetails.filter((row) => row.carID == carID)[0];
+	//   const carDetail = carDetails.filter((row) => row.carID == carID)[0];
 
-  return (
-    <div className='p-[72px] pt-0 flex flex-col gap-[32px]'>
-      <div className='pt-[48px]'></div>
-      {/*  */}
-      {/* <div className='pt-[48px] flex items-center gap-6'>
+	return (
+		<div className='p-[72px] pt-0 flex flex-col gap-[32px]'>
+			<div className='pt-[48px]'></div>
+			{/*  */}
+			{/* <div className='pt-[48px] flex items-center gap-6'>
 				<p className='font-medium'>Model</p>
 				<div className='py-2 px-4 rounded-lg border border-[#CFCFCF]'>
 					<Dropdown
@@ -32,10 +32,10 @@ const SubFullSpec = ({ carID }) => {
 						<Table.HeadCell className='bg-[#30486B] w-[70%] text-white'>code1</Table.HeadCell>
 					</Table.Head>
 					<Table.Body className='divide-y'>
-					<Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
-								<Table.Cell className='whitespace-nowrap font-medium text-gray-900'>description</Table.Cell>
-								<Table.Cell>value</Table.Cell>
-							</Table.Row>
+						<Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+							<Table.Cell className='whitespace-nowrap font-medium text-gray-900'>description</Table.Cell>
+							<Table.Cell>value</Table.Cell>
+						</Table.Row>
 					</Table.Body>
 				</Table>
 			</div>
